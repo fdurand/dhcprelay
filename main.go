1cd6ed616f17d502f13492c3bec182c94caf822a
Durand Fabrice <oeufdure@gmail.com>
2018-11-27 12:53:06 -0500

    First change

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/inverse-inc/packetfence/go/log"
 	dhcp "github.com/krolaw/dhcp4"
 
 	"flag"
@@ -16,12 +21,15 @@ type DHCPHandler struct {
 	m map[string]bool
 }
 
-func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options dhcp.Options) (d dhcp.Packet) {
+func (h *Interface) ServeDHCP(ctx context.Context, p dhcp.Packet, msgType dhcp.MessageType) (answer Answer) {
+	answer.MAC = p.CHAddr()
+	answer.SrcIP = h.Giaddr
+	answer.Iface = h.intNet
 	switch msgType {
 
 	case dhcp.Discover:
 		fmt.Println("discover ", p.YIAddr(), "from", p.CHAddr())
-		h.m[string(p.XId())] = true
+		// h.m[string(p.XId())] = true
 		p2 := dhcp.NewPacket(dhcp.BootRequest)
 		p2.SetCHAddr(p.CHAddr())
 		p2.SetGIAddr(dhcpGIAddr)
@@ -30,12 +38,13 @@ func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options
 		for k, v := range p.ParseOptions() {
 			p2.AddOption(k, v)
 		}
-		return p2
+		answer.D = p2
+		return answer
 
 	case dhcp.Offer:
-		if !h.m[string(p.XId())] {
-			return nil
-		}
+		// if !h.m[string(p.XId())] {
+		// 	return nil
+		// }
 		var sip net.IP
 		for k, v := range p.ParseOptions() {
 			if k == dhcp.OptionServerIdentifier {
@@ -55,10 +64,11 @@ func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options
 		for k, v := range p.ParseOptions() {
 			p2.AddOption(k, v)
 		}
-		return p2
+		answer.D = p2
+		return answer
 
 	case dhcp.Request:
-		h.m[string(p.XId())] = true
+		// h.m[string(p.XId())] = true
 		fmt.Println("request ", p.YIAddr(), "from", p.CHAddr())
 		p2 := dhcp.NewPacket(dhcp.BootRequest)
 		p2.SetCHAddr(p.CHAddr())
@@ -71,12 +81,13 @@ func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options
 		for k, v := range p.ParseOptions() {
 			p2.AddOption(k, v)
 		}
-		return p2
+		answer.D = p2
+		return answer
 
 	case dhcp.ACK:
-		if !h.m[string(p.XId())] {
-			return nil
-		}
+		// if !h.m[string(p.XId())] {
+		// 	return nil
+		// }
 		var sip net.IP
 		for k, v := range p.ParseOptions() {
 			if k == dhcp.OptionServerIdentifier {
@@ -96,12 +107,13 @@ func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options
 		for k, v := range p.ParseOptions() {
 			p2.AddOption(k, v)
 		}
-		return p2
+		answer.D = p2
+		return answer
 
 	case dhcp.NAK:
-		if !h.m[string(p.XId())] {
-			return nil
-		}
+		// if !h.m[string(p.XId())] {
+		// 	return nil
+		// }
 		fmt.Println("NAK from", p.SIAddr(), p.YIAddr(), "to", p.CHAddr())
 		p2 := dhcp.NewPacket(dhcp.BootReply)
 		p2.SetXId(p.XId())
@@ -115,7 +127,8 @@ func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options
 		for k, v := range p.ParseOptions() {
 			p2.AddOption(k, v)
 		}
-		return p2
+		answer.D = p2
+		return answer
 
 	case dhcp.Release, dhcp.Decline:
 		p2 := dhcp.NewPacket(dhcp.BootRequest)
@@ -129,30 +142,93 @@ func (h *DHCPHandler) ServeDHCP(p dhcp.Packet, msgType dhcp.MessageType, options
 		for k, v := range p.ParseOptions() {
 			p2.AddOption(k, v)
 		}
-		return p2
+		answer.D = p2
+		return answer
 	}
-	return nil
+	return answer
 }
 
-func createRelay(in, out string) {
-	handler := &DHCPHandler{m: make(map[string]bool)}
-	go ListenAndServeIf(in, out, 67, handler)
-	ListenAndServeIf(out, in, 68, handler)
+// func createRelay(in, out string) {
+// 	handler := &DHCPHandler{m: make(map[string]bool)}
+// 	go ListenAndServeIf(in, out, 67, handler)
+// 	ListenAndServeIf(out, in, 68, handler)
+// }
+
+type Interface struct {
+	Name   string
+	intNet *net.Interface
+	Giaddr net.IP
 }
+
+var ctx = context.Background()
 
 func main() {
-	flagInInt := flag.String("in", "eth0", "Interface to listen for DHCPv4/BOOTP queries on")
-	flagOutInt := flag.String("out", "eth1", "Outgoing interface (to relay to dhcpservers)")
-	flagServers := flag.String("destination", "", "ip1 ip2 (ip addresses of the dhcpservers")
-	flagBindIP := flag.String("giaddr", "", "required ip address (of outgoing interface and to be used as GIADDR")
+	flagConfig := flag.String("config", "interface:giaddr,interface2:giaddr", "Couple of interface and giaddr, like eth1:192.168.0.1,eth2:192.168.2.1")
 	flag.Parse()
-	servers := strings.Fields(*flagServers)
-	for _, s := range servers {
-		dhcpServers = append(dhcpServers, net.ParseIP(s))
+
+	ctx = log.LoggerNewContext(ctx)
+
+	// Queue value
+	var (
+		maxQueueSize = 100
+		maxWorkers   = 100
+	)
+
+	// create job channel
+	jobs := make(chan job, maxQueueSize)
+
+	// create workers
+	for i := 1; i <= maxWorkers; i++ {
+		go func(i int) {
+			for j := range jobs {
+				doWork(i, j)
+			}
+		}(i)
 	}
-	dhcpGIAddr = net.ParseIP(*flagBindIP)
-	if dhcpGIAddr == nil {
-		panic("giaddr needed")
+	// config := strings.Fields(*flagConfig)
+	result := strings.Split(*flagConfig, ",")
+	for i := range result {
+		spew.Dump(result)
+		interfaceConfig := strings.Split(result[i], ":")
+		iface, _ := net.InterfaceByName(interfaceConfig[0])
+		v := Interface{Name: interfaceConfig[0], intNet: iface, Giaddr: net.ParseIP(interfaceConfig[1])}
+		spew.Dump(v)
+		go func() {
+			v.run(jobs, ctx)
+		}()
+
+		interfaceIP, _ := iface.Addrs()
+		for _, ip := range interfaceIP {
+			go func() {
+				v.runUnicast(jobs, net.ParseIP(ip.String()), ctx)
+			}()
+		}
+		http.ListenAndServe("localhost:6060", nil)
 	}
-	createRelay(*flagInInt, *flagOutInt)
+
+	// servers := strings.Fields(*flagServers)
+	// for _, s := range servers {
+	// 	dhcpServers = append(dhcpServers, net.ParseIP(s))
+	// }
+	// dhcpGIAddr = net.ParseIP(*flagBindIP)
+	// if dhcpGIAddr == nil {
+	// 	panic("giaddr needed")
+	// }
+	// createRelay(*flagInInt, *flagOutInt)
+}
+
+// Broadcast Listener
+func (h *Interface) run(jobs chan job, ctx context.Context) {
+
+	// handler := &DHCPHandler{m: make(map[string]bool)}
+	// go ListenAndServeIf(in, out, 67, handler)
+	// ListenAndServeIf(out, in, 68, handler)
+
+	ListenAndServeIf(h.Name, h, jobs, ctx)
+}
+
+// Unicast listener
+func (h *Interface) runUnicast(jobs chan job, ip net.IP, ctx context.Context) {
+
+	ListenAndServeIfUnicast(h.Name, h, jobs, ip, ctx)
 }
--- server.go
+++ server.go
@@ -1,79 +1,69 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"net"
+
 	dhcp "github.com/krolaw/dhcp4"
 	"golang.org/x/net/ipv4"
-	"net"
-	"strconv"
 )
 
-type serveIfConn struct {
-	ifIndex    int
-	otherIndex int
-	conn       *ipv4.PacketConn
-	cm         *ipv4.ControlMessage
+type Answer struct {
+	D     dhcp.Packet
+	IP    net.IP
+	MAC   net.HardwareAddr
+	Iface *net.Interface
+	SrcIP net.IP
+	Local bool
 }
 
-func (s *serveIfConn) ReadFrom(b []byte) (n int, addr net.Addr, err error) {
-	n, s.cm, addr, err = s.conn.ReadFrom(b)
-	// filter other interfaces
-	if s.cm != nil && s.cm.IfIndex != s.ifIndex && s.cm.IfIndex != s.otherIndex {
-		fmt.Println("dropping packet from ", s.cm.IfIndex)
-		n = 0 // Packets < 240 are filtered in Serve().
-	}
-	return
+type Handler interface {
+	ServeDHCP(ctx context.Context, req dhcp.Packet, msgType dhcp.MessageType) Answer
 }
 
-func (s *serveIfConn) WriteTo(b []byte, addr net.Addr, src net.IP, index int) (n int, err error) {
-	fmt.Println("writing with source", src, index)
-	s.cm.Src = src
-	s.cm.IfIndex = index
-	return s.conn.WriteTo(b, s.cm, addr)
+// ServeConn is the bare minimum connection functions required by Serve()
+// It allows you to create custom connections for greater control,
+// such as ServeIfConn (see serverif.go), which locks to a given interface.
+type ServeConn interface {
+	ReadFrom(b []byte) (n int, addr net.Addr, err error)
+	WriteTo(b []byte, addr net.Addr) (n int, err error)
+	ReadFromRaw(b []byte) (n int, cm *ipv4.ControlMessage, addr net.Addr, err error)
 }
 
-func ListenAndServeIf(interfaceName string, otherName string, port int, handler dhcp.Handler) error {
-	iface, err := net.InterfaceByName(interfaceName)
-	if err != nil {
-		return err
-	}
-	fmt.Println("listen on ", interfaceName, iface.Index, port)
-	other, err := net.InterfaceByName(otherName)
-	if err != nil {
-		return err
-	}
-	p := strconv.Itoa(port)
-	l, err := net.ListenPacket("udp4", ":"+p)
-	if err != nil {
-		return err
-	}
-	defer l.Close()
-	return ServeIf(iface.Index, other.Index, l, handler)
-}
+// Serve takes a ServeConn (such as a net.PacketConn) that it uses for both
+// reading and writing DHCP packets. Every packet is passed to the handler,
+// which processes it and optionally return a response packet for writing back
+// to the network.
+//
+// To capture limited broadcast packets (sent to 255.255.255.255), you must
+// listen on a socket bound to IP_ADDRANY (0.0.0.0). This means that broadcast
+// packets sent to any interface on the system may be delivered to this
+// socket.  See: https://code.google.com/p/go/issues/detail?id=7106
+//
+// Additionally, response packets may not return to the same
+// interface that the request was received from.  Writing a custom ServeConn,
+// or using ServeIf() can provide a workaround to this problem.
+func Serve(conn ServeConn, handler Handler, jobs chan job, ctx context.Context) error {
 
-func ServeIf(ifIndex int, otherIndex int, conn net.PacketConn, handler dhcp.Handler) error {
-	p := ipv4.NewPacketConn(conn)
-	if err := p.SetControlMessage(ipv4.FlagInterface, true); err != nil {
-		return err
-	}
-	return Serve(&serveIfConn{ifIndex: ifIndex, otherIndex: otherIndex, conn: p}, handler)
-}
-
-func Serve(conn *serveIfConn, handler dhcp.Handler) error {
 	buffer := make([]byte, 1500)
+
 	for {
-		n, _, err := conn.ReadFrom(buffer)
+
+		n, cm, addr, err := conn.ReadFromRaw(buffer)
 		if err != nil {
 			return err
 		}
 		if n < 240 { // Packet too small to be DHCP
 			continue
 		}
+
 		req := dhcp.Packet(buffer[:n])
+
 		if req.HLen() > 16 { // Invalid size
 			continue
 		}
 		options := req.ParseOptions()
+
 		var reqType dhcp.MessageType
 		if t := options[dhcp.OptionDHCPMessageType]; len(t) != 1 {
 			continue
@@ -83,17 +73,12 @@ func Serve(conn *serveIfConn, handler dhcp.Handler) error {
 				continue
 			}
 		}
-		if res := handler.ServeDHCP(req, reqType, options); res != nil {
-			// request send upstream
-			if res.OpCode() == 1 {
-				fmt.Println("writing to dhcp server as client (using source port 68 (bootpc))")
-				for _, ip := range dhcpServers {
-					conn.WriteTo(res, &net.UDPAddr{IP: ip, Port: 67}, dhcpGIAddr, conn.otherIndex)
-				}
-			} else {
-				fmt.Println("writing to client as server (using source port 67 (bootps))")
-				conn.WriteTo(res, &net.UDPAddr{IP: net.IPv4bcast, Port: 68}, dhcpGIAddr, conn.ifIndex)
-			}
-		}
+		var dhcprequest dhcp.Packet
+		dhcprequest = append([]byte(nil), req...)
+		jobe := job{dhcprequest, reqType, handler, addr, cm.Dst, ctx}
+		go func() {
+			jobs <- jobe
+		}()
+
 	}
 }
